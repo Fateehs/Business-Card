54fbfcb3a46bd2e09e8f80e04c8bfa10a0b65dd5
Fateehs <benzer1010@gmail.com>
2023-09-18 17:45:46 +0300

    18.09.2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -12,10 +11,6 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
   },
 ];
 
