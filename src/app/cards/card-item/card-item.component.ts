7800ef86117d4c401f160efdf3285d54ffb24193
Fateehs <benzer1010@gmail.com>
2023-09-20 17:57:04 +0300

    20.09.2023

---
+++ src/app/cards/card-item/card-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Card } from 'src/app/models/card';
+
+@Component({
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.scss']
+})
+export class CardItemComponent {
+ @Input() card!: Card;
+}
